f808eb1467c839912ec033a860956e4848081889
Sam Soffes <sam@soff.es>
2014-12-02 12:38:52 -0800

    Add model

---
+++ db/migrate/20141202203706_create_episodes.rb
@@ -0,0 +1,27 @@
+class CreateEpisodes < ActiveRecord::Migration
+  def change
+    create_table :episodes do |t|
+      t.integer :order, null: false
+      t.datetime :published_at
+      t.string :title, null: false
+      t.text :description
+      t.text :description_html
+      t.string :tags
+      t.integer :duration
+      t.integer :vimeo_id
+      t.string :hd_video_url
+      t.string :hd_content_type
+      t.integer :hd_file_size
+      t.string :hd_poster_url
+      t.string :sd_video_url
+      t.string :sd_content_type
+      t.integer :sd_file_size
+      t.string :sd_poster_url
+
+      t.timestamps
+    end
+
+    add_index :episodes, :order, unique: true
+    add_index :episodes, :published_at
+  end
+end

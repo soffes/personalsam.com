9391a88cf0e0185aff0af9c2bce114fd0014f15b
Sam Soffes <sam@soff.es>
2014-12-04 01:32:03 -0800

    Add embed

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
-VIMEO_ACCESS_TOKEN = ENV['VIMEO_ACCESS_TOKEN']
-MIXPANEL_TOKEN = ENV['MIXPANEL_TOKEN']
-DOWNLOAD_SHARED_SECRET = ENV['DOWNLOAD_SHARED_SECRET']
+%w{VIMEO_ACCESS_TOKEN MIXPANEL_TOKEN DOWNLOAD_SHARED_SECRET}.each do |key|
+  raise "Required environment variable `#{key}` is missing." unless value = ENV[key]
+  Kernel.const_set key, value
+end
